f2058572519d954e3cada7ead9628abaafae874a
surya180857 <surya180857@gmail.com>
2024-10-24 19:55:09 +0530

    button changed to as per requirment i.e to accent color and foregroundStyle (Black)

--- cycle companion Watch App/ContentView.swift
+++ cycle companion Watch App/ContentView.swift
@@ -9,16 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Start"){
+            // excute the application
         }
-        .padding()
+            .buttonStyle(.borderedProminent)
+            .tint(Color("AccentColor"))
+            .foregroundStyle(.black)
     }
 }
-
 #Preview {
     ContentView()
 }
